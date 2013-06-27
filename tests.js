a3a1c77ff69db3b87df81f5903af1ea595c7f64e
Jeremy Strawn <jkstrawn@gmail.com>
2013-06-27 16:22:43 -0400

    Tests up to: choosing the last empty space

---
+++ tests.js
@@ -0,0 +1,84 @@
+describe('tic tac toe', function() {
+	var manager;
+
+	beforeEach(function() {
+		manager = new Manager();
+	});
+
+	it('has a token on the top left square of the board when X chooses that location', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('X', 0, 0);
+		expect(manager.getTokenAtSquare(0, 0)).toEqual("X");
+	});
+
+	it('has an X token on the middle square of the board when X chooses that location', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('X', 1, 1);
+		expect(manager.getTokenAtSquare(1, 1)).toEqual("X");
+	});
+
+	it('has an O token on the middle square of the board when O chooses that location', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('O', 1, 1);
+		expect(manager.getTokenAtSquare(1, 1)).toEqual("O");
+	});
+
+	it('interprets no win with only one X token on the board', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('X', 0, 0);
+		manager.placeTokenOnSquare('O', 0, 1);
+		expect(manager.isThereAWinner()).toEqual(false);
+	});
+
+	it('interprets X as the winner if X has three tokens on top', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('X', 0, 0);
+		manager.placeTokenOnSquare('X', 0, 1);
+		manager.placeTokenOnSquare('X', 0, 2);
+		expect(manager.isThereAWinner()).toEqual("X");
+	});
+
+	it('interprets X as the winner if X has three tokens on bottom', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('X', 2, 0);
+		manager.placeTokenOnSquare('X', 2, 1);
+		manager.placeTokenOnSquare('X', 2, 2);
+		expect(manager.isThereAWinner()).toEqual("X");
+	});
+
+	it('interprets X as the winner if X has reverse diagonal', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('X', 0, 2);
+		manager.placeTokenOnSquare('X', 1, 1);
+		manager.placeTokenOnSquare('X', 2, 0);
+		expect(manager.isThereAWinner()).toEqual("X");
+	});
+
+	it('places the computer token in the middle if it is the last spot (depth: 0)', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('O', 0, 2);
+		manager.placeTokenOnSquare('X', 1, 2);
+		manager.placeTokenOnSquare('O', 0, 1);
+		manager.placeTokenOnSquare('X', 0, 0);
+		manager.placeTokenOnSquare('O', 1, 0);
+		manager.placeTokenOnSquare('X', 2, 0);
+		manager.placeTokenOnSquare('O', 2, 2);
+		manager.placeTokenOnSquare('X', 2, 1);
+		manager.makeComputerMove();
+		expect(manager.getTokenAtSquare(1, 1)).toEqual('O');
+	});
+/*
+	it('places the computer token in the winning square with 2 squares open (depth: 1)', function() {
+		manager.initiate();
+		manager.placeTokenOnSquare('X', 2, 0);
+		manager.placeTokenOnSquare('O', 1, 1);
+		manager.placeTokenOnSquare('X', 2, 2);
+		manager.placeTokenOnSquare('O', 2, 1);
+		manager.placeTokenOnSquare('X', 0, 1);
+		manager.placeTokenOnSquare('O', 1, 2);
+		manager.placeTokenOnSquare('X', 0, 2);
+		manager.makeComputerMove();
+		expect(manager.isThereAWinner()).toEqual('O');
+	});
+*/
+});
---
+++ tictactoe.js
@@ -0,0 +1,104 @@
+var Manager = function() {
+	this.board = [];
+
+	this.initiate = function() {
+		this.board = new Board();
+		this.board.initialize();
+	}
+
+	this.placeTokenOnSquare = function(token, x, y) {
+		this.board.setToken(token, x, y);
+	}
+
+	this.getTokenAtSquare = function(x, y) {
+		return this.board.getToken(x, y);
+	}
+
+	this.makeComputerMove = function() {
+		var emptySquares = this.board.forEach(function(square){
+			if (square.isEmpty()) {
+				return square;
+			}
+			return false;
+		});
+		if (emptySquares.length == 1) {
+			emptySquares[0].setToken("O");
+		}
+	}
+
+	this.isThereAWinner = function() {
+		return this.board.isThereAWinner();
+	}
+}
+
+var Square = function(x, y) {
+	this.x = x;
+	this.y = y;
+	this.token = '';
+
+	this.isEmpty = function() {
+		if (this.token == '') {
+			return true;
+		}
+		return false;
+	}
+
+	this.setToken = function(token) {
+		this.token = token;
+	}
+}
+
+var Board = function() {
+	this.squares = [];
+
+	this.setToken = function(token, x, y) {
+		this.squares[x][y].setToken(token);
+	}
+
+	this.getToken = function(x, y) {
+		return this.squares[x][y].token;
+	}
+
+	this.initialize = function() {
+		for (var x = 0; x < 3; x++) {
+			this.squares[x] = [];
+			for (var y = 0; y < 3; y++) {
+				this.squares[x][y] = new Square(x, y);
+			}
+		}
+	}
+
+	this.forEach = function(doForEach) {
+		var squares = [];
+		for (var x = 0; x < 3; x++) {
+			for (var y = 0; y < 3; y++) {
+				var returned = doForEach(this.squares[x][y]);
+				if (returned) squares.push(returned);
+			}
+		}
+		return squares;
+	}
+
+	this.isThereAWinner = function() {
+		var winner = '';
+		var rows = [];
+		rows[0] = [this.squares[0][0].token, this.squares[0][1].token, this.squares[0][2].token];
+		rows[1] = [this.squares[1][0].token, this.squares[1][1].token, this.squares[1][2].token];
+		rows[2] = [this.squares[2][0].token, this.squares[2][1].token, this.squares[2][2].token];
+		rows[3] = [this.squares[0][0].token, this.squares[1][0].token, this.squares[2][0].token];
+		rows[4] = [this.squares[0][1].token, this.squares[1][1].token, this.squares[2][1].token];
+		rows[5] = [this.squares[0][2].token, this.squares[1][2].token, this.squares[2][2].token];
+		rows[6] = [this.squares[0][0].token, this.squares[1][1].token, this.squares[2][2].token];
+		rows[7] = [this.squares[0][2].token, this.squares[1][1].token, this.squares[2][0].token];
+
+		for (var i = 0; i < rows.length; i++) {
+			if (rows[i][1] == rows[i][0] && rows[i][2] == rows[i][0] && rows[i][0] != '') {
+				winner = rows[i][0];
+			}
+		}
+		if (winner.length) {
+			return winner;
+		}
+		return false;
+	}
+}
